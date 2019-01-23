d896506f7e09c1e49e4e8f43f24ff0bf63391a97
Henrique Varella Ehrenfried <h.v.ehrenfried@hotmail.com>
2019-01-23 08:27:44 -0200

    Add license to the files

---
+++ database/__init__.py
@@ -0,0 +1,21 @@
+"""
+Copyright (C) 2018 Centro de Computacao Cientifica e Software Livre
+Departamento de Informatica - Universidade Federal do Parana - C3SL/UFPR
+
+This file is part of HOTMapper.
+
+HOTMapper is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+HOTMapper is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with simcaq-cdn.  If not, see <https://www.gnu.org/licenses/>.
+
+"""
+
